957c65ae5b0bd25f03d14030782cda993684715b
jamesbieber <jamesbieber97@gmail.com>
2019-12-24 08:48:20 -0800

    Updates

---
+++ precache-manifest.ebbf7a39c617e2cee6efe53e07031162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c3f688e03aa06f209c96940f5d90be",
+    "url": "/POS-Mock/index.html"
+  },
+  {
+    "revision": "96620562e7c35391b4be",
+    "url": "/POS-Mock/static/css/main.46e5a523.chunk.css"
+  },
+  {
+    "revision": "1610bdc9032076d521ca",
+    "url": "/POS-Mock/static/js/2.3404fc13.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/POS-Mock/static/js/2.3404fc13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96620562e7c35391b4be",
+    "url": "/POS-Mock/static/js/main.b4fdd88b.chunk.js"
+  },
+  {
+    "revision": "f32b837b29a4fee0a417",
+    "url": "/POS-Mock/static/js/runtime-main.6d95ae6d.js"
+  }
+]);
